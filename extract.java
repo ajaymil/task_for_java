291dc30159e376477d0f84ace054f5b360f9d2fd
ajaymil <118195266+ajaymil@users.noreply.github.com>
2023-10-31 05:03:03 -0700

    task4

---
+++ extract.java
@@ -0,0 +1,7 @@
+public class extract{
+public static void main(String[] args) {
+    String main_str="hello how are you";
+    String sub_str="how";
+    System.out.println(main_str.contains(sub_str));
+}
+}
