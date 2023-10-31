b14e0495a4069a56538e50146cad337d4bf15b19
ajaymil <118195266+ajaymil@users.noreply.github.com>
2023-10-31 04:59:06 -0700

    Add files via upload
    task-1

---
+++ task.java
@@ -0,0 +1,18 @@
+public class task {
+    public static void main(String[] args) {
+        String s1,s2;
+        s1="apple";
+        s2="Apple";
+    if(s1.equalsIgnoreCase(s2)){
+        System.out.println("equal");
+
+
+    }
+    else{
+        System.out.println("not equal");
+    }
+
+
+    }
+    
+}
