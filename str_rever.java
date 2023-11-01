9f4f608228deec5a811422c1c9e45487870e28ac
ajaymil <118195266+ajaymil@users.noreply.github.com>
2023-11-01 02:15:17 -0700

    reverse_funct

---
+++ str_rever.java
@@ -0,0 +1,16 @@
+public class str_rever {
+       static void fuct(String a){
+for(int i=a.length()-1;i>=0;i--){
+    System.out.print(a.charAt(i));
+}
+
+return;
+       }
+    public static void main(String[] args) {
+fuct("hello java");
+
+    }
+    
+    
+
+}
