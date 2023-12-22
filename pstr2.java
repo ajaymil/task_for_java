d4ddf719ad9e7dc3df3254548f988c11e7e05c5b
ajaymil <118195266+ajaymil@users.noreply.github.com>
2023-12-22 04:31:23 -0700

    final output

--- pstr2.java
+++ pstr2.java
@@ -1,73 +1,25 @@
 import java.util.Scanner;
-class user_ip{
-    public String enter_String(){
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.print(" enter String :");
-          String str1 = sc.nextLine();
- return str1;
-    }
 
-  public char enter_char(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter the letter:");
-       char ch2 = sc.next().charAt(0);
- return ch2;
-  }
-    
-}
+
+
 class fun {
-    public void st_fun(){
-        user_ip user=new user_ip();
-       String str1=user.enter_String();
-       char ch2=user.enter_char();
-      /* System.out.println(str1);
-       System.out.println(ch2);
-       */
-      int c = 0;
-         for(int i=0;i<str1.length();i++)
-         {
-              if(str1.charAt(i)==ch2)
-            {
+    public void st_fun(String str1,char ch2) {
+        
+        int c = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == ch2) {
                 System.out.println(i);
                 c++;
-                 for( int j=0;j>=i;j++)
-                 {
-                  
-                  
-                    //System.out.println(j);
-                    break;
-
-
-                 }
-                                
             }
-           /* else
-            {
-                System.out.println("letter not found ");
-                break;
+        }
 
-            }*/
-           
-            // System.out.println("no.of .letter +ch2"+"is "+c );
-           //  continue;
-         }
-
-         System.out.println("no.of .letter " +ch2+ " is "+c );
-        
-        
-
+        System.out.println("Number of letter " + ch2 + " is " + c);
     }
 }
-/*class mention{
-    public void men(){
-user_ip user=new user_ip();
-       String str1=user.enter_String();
-       char ch2=user.enter_char();
 
-         System.out.println(str1);
-         System.out.println("\n");
-         int j=0;
+class mention {
+    public void men(String str1,char ch2) {
+        int j=0;
            for(int i=0;i<str1.length();i++){
          //   System.out.println(str1.charAt(i));
              while(j<str1.length()){
@@ -77,7 +29,7 @@ user_ip user=new user_ip();
                 continue;
                             
                 }
-          System.out.print(str1.charAt(j)+" ");
+          System.out.println(str1.charAt(j));
 
 j++;
                     
@@ -88,18 +40,52 @@ j++;
            //  continue;
          }
 
-
-
-
     }
-}*/
+}
+class non{
+  public void nonmension(String str1,char ch2)
+  {
+   int cot=0;
+  for (int s = 0; s < str1.length(); s++){
+       if (str1.charAt(s) == ch2) {
+         continue;
+               
+            }
+            
+            
+            
+    
+    
+ for( int b=0;b<=s;b++){
+                     if(str1.charAt(s) == ch2)   
+                       {                              //System.out.println(j);
+                    break;
+                                }
+                               
+                               
+
+                            }
+                                cot++;
+        }
+     
+        System.out.println(cot);
+  }
+}
+
 public class pstr2 {
     public static void main(String[] args) {
-        fun nn=new fun();
-        nn.st_fun();
-       /*  mention ns=new mention();
-        ns.men();*/
+      Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+         String str1= sc.nextLine();
+         System.out.print("Enter the letter: ");
+        char ch2=sc.next().charAt(0); 
 
+         fun nn = new fun();
+        nn.st_fun(str1,ch2);
+
+        mention ns = new mention();
+        ns.men(str1,ch2);
+        non w=new non();
+        w.nonmension(str1, ch2);
     }
-    
 }
